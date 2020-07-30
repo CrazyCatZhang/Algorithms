9dfe53c3a095d2df83c3cc7db86c5ac7b8e9810d
Peach Zhang <CrazyCatZhang@outlook.com>
2020-07-30 20:53:29 +0800

    Update QuickSort.java

--- Algorithms&Datastructures/Algorithms/src/com/catzhang/sort/QuickSort.java
+++ Algorithms&Datastructures/Algorithms/src/com/catzhang/sort/QuickSort.java
@@ -9,36 +9,36 @@ import java.util.Arrays;
  */
 public class QuickSort {
     public static void main(String[] args) {
-        int [] arr = {-9,78,0,23,-567,70,-1,900,4561};
-        quickSort(arr,0,arr.length - 1);
+        int[] arr = {-9, 78, 0, 23, -567, 70, -1, 900, 4561};
+        quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
     //Define QuickSort method
-    public static void quickSort(int [] arr,int low,int high){
+    public static void quickSort(int[] arr, int low, int high) {
         int left = low;
         int right = high;
         int pivot = arr[(high + low) / 2];
-        while (left < right){
-            while ((left < right) && arr[right] > pivot){
+        while (left < right) {
+            while ((left < right) && arr[right] > pivot) {
                 right--;
             }
-            while ((left < right) && arr[left] < pivot){
+            while ((left < right) && arr[left] < pivot) {
                 left++;
             }
-            if (arr[left] == arr[right] && left < right){
+            if (arr[left] == arr[right] && left < right) {
                 left++;
-            }else {
+            } else {
                 int temp = arr[left];
                 arr[left] = arr[right];
                 arr[right] = temp;
             }
         }
-        if (left - 1 > low){
-            quickSort(arr,low,left - 1);
+        if (left - 1 > low) {
+            quickSort(arr, low, left - 1);
         }
-        if (right + 1 < high){
-            quickSort(arr,right + 1,high);
+        if (right + 1 < high) {
+            quickSort(arr, right + 1, high);
         }
     }
 }
