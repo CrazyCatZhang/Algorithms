3e4dd538293e431612eb33662db47b5498d1e54e
Peach Zhang <CrazyCatZhang@outlook.com>
2020-07-30 23:53:41 +0800

    Update RadixSort.java

--- Algorithms&Datastructures/Algorithms/src/com/catzhang/sort/RadixSort.java
+++ Algorithms&Datastructures/Algorithms/src/com/catzhang/sort/RadixSort.java
@@ -9,21 +9,21 @@ import java.util.Arrays;
  */
 public class RadixSort {
     public static void main(String[] args) {
-        int [] arr = {53,3,542,748,14,214};
+        int[] arr = {53, 3, 542, 748, 14, 214};
         radixSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     //Define RadixSort method
-    public static void radixSort(int [] arr){
+    public static void radixSort(int[] arr) {
         int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
             max = max > arr[i] ? max : arr[i];
         }
         int maxLength = (max + "").length();
-        int [][] bucket = new int[10][arr.length];
-        int [] bucketElementCounts = new int[10];
-        for (int i = 0,n = 1; i < maxLength; i++,n *= 10) {
+        int[][] bucket = new int[10][arr.length];
+        int[] bucketElementCounts = new int[10];
+        for (int i = 0, n = 1; i < maxLength; i++, n *= 10) {
             for (int j = 0; j < arr.length; j++) {
                 int digitOfElement = arr[j] / n % 10;
                 bucket[digitOfElement][bucketElementCounts[digitOfElement]] = arr[j];
@@ -31,7 +31,7 @@ public class RadixSort {
             }
             int index = 0;
             for (int j = 0; j < bucket.length; j++) {
-                if (bucketElementCounts[j] != 0){
+                if (bucketElementCounts[j] != 0) {
                     for (int k = 0; k < bucketElementCounts[j]; k++) {
                         arr[index++] = bucket[j][k];
                     }
