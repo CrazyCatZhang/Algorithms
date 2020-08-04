d0bf84ffb1b2fcff6f5b01aefc02f7110d43f362
Peach Zhang <CrazyCatZhang@outlook.com>
2020-08-04 20:09:11 +0800

    Create HanoiTower.java
    Realize the Tower of Hanoi through divide and conquer

---
+++ Algorithms&Datastructures/CommonAlgorithms/src/com/catzhang/divideandconquer/HanoiTower.java
@@ -0,0 +1,28 @@
+package com.catzhang.divideandconquer;
+
+/**
+ * @author: crazycatzhang
+ * @date: 2020/8/4 7:26 PM
+ * @description: Realize the Tower of Hanoi through divide and conquer
+ */
+public class HanoiTower {
+    public static void main(String[] args) {
+        hanoiTower(5,'A','B','C');
+        System.out.println(count);
+    }
+
+    private static int count = 0;
+
+    //Define the DAC method
+    public static void hanoiTower(int num, char a, char b, char c) {
+        if (num == 1){
+            System.out.println("The 1 plate from " + a + " to " + c);
+            count++;
+        }else {
+            hanoiTower(num - 1,a,c,b);
+            System.out.println("The " + num + " plate from " + a + " to " + c);
+            count++;
+            hanoiTower(num - 1,b,a,c);
+        }
+    }
+}
