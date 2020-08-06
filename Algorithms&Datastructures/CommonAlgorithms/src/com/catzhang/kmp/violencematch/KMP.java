1b1f52f1aca7600a6be62542c133b93dfb2c5cd0
Peach Zhang <CrazyCatZhang@outlook.com>
2020-08-06 16:09:46 +0800

    Create KMP.java
    Realize string matching through kmp algorithm

---
+++ Algorithms&Datastructures/CommonAlgorithms/src/com/catzhang/kmp/violencematch/KMP.java
@@ -0,0 +1,49 @@
+package com.catzhang.kmp.violencematch;
+
+import java.util.Arrays;
+
+/**
+ * @author: crazycatzhang
+ * @date: 2020/8/6 10:18 AM
+ * @description: Realize string matching through kmp algorithm
+ */
+public class KMP {
+    public static void main(String[] args) {
+        String str1 = "BBC ABCDAB ABCDABCDABDE";
+        String str2 = "ABCDABD";
+        int[] partialMatchTable = partialMatchTable(str2);
+        System.out.println(kmpSearch(str1, str2, partialMatchTable));
+    }
+
+    //Define KMP method
+    public static int kmpSearch(String str1, String str2, int[] partialMatchTable) {
+        for (int i = 0, j = 0; i < str1.length(); i++) {
+            while (j > 0 && str1.charAt(i) != str2.charAt(j)) {
+                j = partialMatchTable[j - 1];
+            }
+            if (str1.charAt(i) == str2.charAt(j)) {
+                j++;
+            }
+            if (j == str2.length()) {
+                return i - j + 1;
+            }
+        }
+        return -1;
+    }
+
+    //Get the partial matching value table of the string
+    public static int[] partialMatchTable(String dest) {
+        int[] next = new int[dest.length()];
+        next[0] = 0;
+        for (int i = 1, j = 0; i < dest.length(); i++) {
+            while (j > 0 && dest.charAt(i) != dest.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (dest.charAt(i) == dest.charAt(j)) {
+                j++;
+            }
+            next[i] = j;
+        }
+        return next;
+    }
+}
